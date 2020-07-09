7123b65e77ae24fe4597ee76097614573eed0eae
Gary Tou <garytou2@gmail.com>
2020-07-09 02:26:35 -0700

    Added support for multiple types of certificates

--- javascript.js
+++ javascript.js
@@ -1,80 +1,98 @@
 function checkCert() {
-  const urlParams = new URLSearchParams(window.location.search);
-  const name = urlParams.get("name");
-  const role = urlParams.get("role");
-  const hash = urlParams.get("key");
+   const certType = ["hackthecloud"];
 
-  console.log(urlParams);
-  console.log(name);
-  console.log(role);
-  console.log(hash);
+   const urlParams = new URLSearchParams(window.location.search);
+   const name = urlParams.get("name");
+   const role = urlParams.get("role");
+   const type = urlParams.get("type");
+   const hash = urlParams.get("key");
 
-  var combined = name + role;
+   var combined = name + role + type;
 
-  var correctHash = 0;
-  if (combined.length != 0) {
-    for (i = 0; i < combined.length; i++) {
-      char = combined.charCodeAt(i);
-      correctHash = (correctHash << 5) - correctHash + char;
-      correctHash = correctHash & correctHash;
-    }
-  }
-  correctHash += ""; //turn into string
-  console.log("correctHash: " + correctHash);
-
-  if (correctHash == hash) {
-    //wait until elements exist
-    var observer = new MutationObserver(function (mutations, me) {
-      var elements = [
-        document.getElementById("cert"),
-        document.getElementById("cert-temp"),
-      ];
-      var missing = false;
-      for (var i = 0; i < elements.length; i++) {
-        if (!elements[i]) {
-          missing = true;
-        }
+   var correctHash = 0;
+   if (combined.length != 0) {
+      for (var i = 0; i < combined.length; i++) {
+         var char = combined.charCodeAt(i);
+         correctHash = (correctHash << 5) - correctHash + char;
+         correctHash = correctHash & correctHash;
       }
-      if (!missing) {
-        var certHolder = document.getElementById("cert");
-        var cert = document.getElementById("cert-temp").content.cloneNode(true);
-        certHolder.innerHTML = "";
-        certHolder.appendChild(cert);
-        document.getElementById("name").innerText = name;
-        document.getElementById("role").innerText = role;
+   }
+   correctHash += ""; //turn into string
 
-        destroyCertTemp();
-        me.disconnect(); // stop observing
-        return;
-      }
-    });
+   if (!(name || role || type || hash)) {
+      console.log("no params");
+      window.onload = function () {
+         homePage();
+         destroyCertTemp();
 
-    observer.observe(document, {
-      childList: true,
-      subtree: true,
-    });
-  } else {
-    console.log("incorrect");
-    window.onload = function () {
-      var certHolder = document.getElementById("cert");
-      var error = document.getElementById("error").content.cloneNode(true);
-      certHolder.innerHTML = "";
-      certHolder.appendChild(error);
+         var fab = document.getElementsByClassName("fab")[0];
+         fab.style.display = "none";
+      };
+   } else if (correctHash == hash && certType.includes(type)) {
+      //wait until elements exist
+      var observer = new MutationObserver(function (mutations, me) {
+         var elements = [
+            document.getElementById("cert")
+         ];
+         var missing = false;
+         for (var i = 0; i < elements.length; i++) {
+            if (!elements[i]) {
+               missing = true;
+            }
+         }
+         if (!missing) {
+            var path = "certs/" + type + ".html";
+            $("#cert").load(path, function () {
+					document.getElementById("name").innerText = name;
+               document.getElementById("role").innerText = role;
+               destroyCertTemp();
+               console.log("loaded cert");
+				});
 
-		destroyCertTemp();
-		
-		var fab = document.getElementsByClassName("fab")[0];
-		fab.style.display = "none";
-    };
-  }
+            me.disconnect();
+            return;
+         }
+      });
+
+      observer.observe(document, {
+         childList: true,
+         subtree: true,
+      });
+   } else {
+      console.log("invalid cert");
+      window.onload = function () {
+         invalidCert();
+         destroyCertTemp();
+
+         var fab = document.getElementsByClassName("fab")[0];
+         fab.style.display = "none";
+      };
+   }
+}
+
+function homePage() {
+   var certHolder = document.getElementById("cert");
+   var error = document.getElementById("home").content.cloneNode(true);
+   certHolder.innerHTML = "";
+   certHolder.appendChild(error);
+}
+
+function invalidCert() {
+   var certHolder = document.getElementById("cert");
+   var error = document.getElementById("error").content.cloneNode(true);
+   certHolder.innerHTML = "";
+   certHolder.appendChild(error);
 }
 
 function destroyCertTemp() {
-  document.getElementById("cert-temp").innerHTML = "";
+   var certDesigns = document.getElementsByClassName("cert-design");
+   for (var i = 0; i < certDesigns.length; i++) {
+      certDesigns[i].parentNode.removeChild(certDesigns[i]);
+   }
 }
 
 function download() {
-  window.print();
+   window.print();
 }
 
 checkCert();
